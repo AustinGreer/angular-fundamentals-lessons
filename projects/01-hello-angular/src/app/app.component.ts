87248203e9f9735f4f86fcd597245962c76b5861
Austin Greer <agreer.ag12@gmail.com>
2025-02-17 21:54:06 -0500

    added some changes for the first two projects

--- projects/01-hello-angular/src/app/app.component.ts
+++ projects/01-hello-angular/src/app/app.component.ts
@@ -9,7 +9,12 @@ import { RouterOutlet } from '@angular/router';
   template: `
     <h1>If you are reading this...</h1>
     <p>Things have worked out well! 🎉</p>
+
+    <ol>
+      <li>Hello</li>
+      <li>World</li>
+    </ol>
   `,
   styles: ``,
 })
-export class AppComponent {}
+export class AppComponent { }
