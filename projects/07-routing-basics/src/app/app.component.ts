535400d0c4b91ec1f7a4fad8db4b8559e8a2dfdd
Austin Greer <agreer.ag12@gmail.com>
2025-03-08 23:21:36 -0600

    adds code for basic routing module

--- projects/07-routing-basics/src/app/app.component.ts
+++ projects/07-routing-basics/src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
-  template: ` <h1>Enable routing to see the greeting below</h1> `,
+  imports: [CommonModule, RouterModule], // you need RouterModule if you are going to use anything in relation to router (i.e. router-outlet)
+  template: `
+    <h1>Enable routing to see the greeting below</h1>
+    <router-outlet />
+  `,
   styles: [],
 })
 export class AppComponent {
--- projects/07-routing-basics/src/app/app.routes.ts
+++ projects/07-routing-basics/src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
+import { GreetingsComponent } from './greetings.component';
 
 export const routes: Routes = [
   {
     // add a default path to greetings component
+    // yuou can do some stuff like loadComp0nent and loadChildren for lazy loading implementation
+    // you can also add advanced routes and private routes in this layer.
+    path: "",
+    component: GreetingsComponent
   },
 ];
