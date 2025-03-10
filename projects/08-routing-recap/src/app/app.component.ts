32e8c6f694b897b2bba9561ff48c158ccc97c06d
Austin Greer <agreer.ag12@gmail.com>
2025-03-09 23:09:15 -0500

    adds routing recap

--- projects/08-routing-recap/src/app/app.component.ts
+++ projects/08-routing-recap/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: ` <h1>Welcome to {{ title }}!</h1> `,
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <h1>Welcome to {{ title }}!</h1>
+    <router-outlet />
+    @for(product of productTitles; track product) {
+      <div>
+        <a [routerLink]="['details', $index]">{{product}}</a>
+      </div>
+    }
+  `,
 })
 export class AppComponent {
   title = '08-routing-recap';
